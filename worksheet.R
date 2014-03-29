f7c88b0594ea69df39ab73d3379485929a7eee6a
Brian To <bxt5647@rit.edu>
2014-03-29 05:21:54 -0400

    Got sample timeline up!

--- worksheet.R
+++ worksheet.R
@@ -1,12 +1,32 @@
-durations <- 1 + rpois(n = 10, lambda = 3)
-offsets <- sample(0:5, size = 10, replace = TRUE)
-ends <- offsets + durations
-event <- sapply(ends, function(end) { return(end < 8) })
-timeline <- data.frame(start = offsets, end = ends, event = event)
+timeline <- read.csv('example-timeline.csv', header=TRUE, row.names=1)
 
 # Study ends at t = 7
 survival <- survfit(Surv(start, end, event) ~ 1, data = timeline)
 
-timeline.plot <-
-  ggplot(timeline) +
-  geom_segment(aes(x = start, xend = end, y = rownames(timeline), yend = rownames(timeline)))
+timeline.plot <- ggplot(timeline) +
+  xlab('time') +
+  ylab('observation') +
+  labs(
+    colour = "Observation",
+    shape = "Event Occured?")+
+  geom_segment(aes(
+    colour = rownames(timeline),
+    x = start,
+    xend = end,
+    y = rownames(timeline),
+    yend = rownames(timeline))) +
+  geom_point(aes(
+    colour = rownames(timeline),
+    x = start,
+    y = rownames(timeline)), size = 4, title="asdf") +
+  geom_point(aes(
+    colour = rownames(timeline),
+    shape = event,
+    x = end,
+    y = rownames(timeline)), size = 4) +
+  geom_vline(
+    xintercept = 6,
+    colour = 'red',
+    linetype = 'longdash')
+
+timeline.plot
